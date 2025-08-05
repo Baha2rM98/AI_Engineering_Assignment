075aa0619d8813642c693fdaf7bfe53c5cd282f2
ateferos77 <atefe.rostami@studenti.unipd.it>
2025-08-05 22:33:49 +0200

    Update main.py to set default port and host from environment variables

--- app/main.py
+++ app/main.py
@@ -7,12 +7,13 @@ load_dotenv()
 
 if __name__ == "__main__":
     # Get port from environment or default to 8000
-    port = int(os.getenv("PORT"))
+    port = int(os.getenv("PORT", "8000"))
+    host = os.getenv("HOST", "0.0.0.0")
 
     # Run the application
     uvicorn.run(
         "app.api.routes:app",
-        host=os.getenv("HOST"),
+        host=host,
         port=port,
         reload=True
     )
